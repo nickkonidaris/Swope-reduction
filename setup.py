92606391c2620a266ea26236f7dacc52ad944d42
Nick Konidaris <nick.konidaris@gmail.com>
2018-02-09 15:58:01 -0300

    Change from default

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 import os
 from setuptools import setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "empty_python",
+    name = "Swoper",
     version = "0.0.1",
-    author = "Ben van Werkhoven",
-    author_email = "b.vanwerkhoven@esciencecenter.nl",
-    description = ("An empty Python project"),
-    license = "Apache 2.0",
+    author = "Nicholas P. Konidaris",
+    author_email = "npk@carnegiescience.edu",
+    description = ("Swope data reduction"),
+    license = "GNU General Public License v3.0",
     keywords = "Python",
-    url = "https://github.com/benvanwerkhoven/empty_python",
-    packages=['empty_python'],
+    url = "https://github.com/nickkonidaris/Swope-reduction",
+    packages=['Swoper'],
     long_description=read('README.md'),
     classifiers=[
         'Development Status :: 1 - Planning',
-        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
     ],
 )
-
