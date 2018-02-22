2c0390bcc870d7f9d87f10a0968a6e0f690c0b68
Nick Konidaris <nick.konidaris@gmail.com>
2018-02-21 18:52:03 -0800

    created

---
+++ Swoper/handle_imcombine.py
@@ -0,0 +1,42 @@
+import argparse
+import astropy
+import astropy.io.fits
+import ccdproc
+
+import os
+from ccdproc import ImageFileCollection
+from ccdproc import wcs_project
+
+astropy.log.setLevel("ERROR")
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", default=".", help="glob to exclude")
+    args = parser.parse_args()
+
+    for channel in ["c1", "c2", "c3", "c4"]:
+        ic = ImageFileCollection(args.path, glob_include="wcs*%s*fits" % \
+                                 channel)
+        first = None
+        reprojected = []
+        for img, fname in ic.ccds(return_fname=True):
+            print(fname)
+            if first is None:
+                first = img
+                print("New")
+                reprojected.append(first)
+                continue
+
+            new = wcs_project(img, first.wcs, order="nearest-neighbor")
+            try: new.write("rp_%s" % fname)
+            except OSError: continue
+            reprojected.append(new)
+
+        combiner = ccdproc.Combiner(reprojected)
+        stacked_image = combiner.average_combine()
+        first.data = stacked_image.data
+        try: first.write("%s_stack.fits" % channel)
+        except OSError: continue
+
